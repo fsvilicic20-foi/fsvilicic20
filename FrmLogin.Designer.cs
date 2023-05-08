c4eb364be34990064ffd17e34c388487340fedb3
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-08 19:05:05 +0200

    vjezba 3

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -107,10 +107,5 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnLogin;
-    }
+        
 }
