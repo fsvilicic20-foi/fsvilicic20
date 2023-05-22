84d6a48876cc4fb5cd1dbc2b43d936c4462ec3d5
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-22 20:21:01 +0200

    Prikaz forme sa izvještajem

---
+++ FrmFinalReport.cs
@@ -0,0 +1,15 @@
+﻿private List<StudentReportView> GenerateStudentReport() {
+    var allStudents = StudentRepository.GetStudents();
+    var examReports = new List<StudentReportView>();
+    foreach (var student in allStudents) {
+        if (student.HasSignature() == true) {
+            var examReport = new StudentReportView(student);
+            examReports.Add(examReport);
+        }
+    }
+    return examReports;
+}private void FrmFinalReport_Load(object sender, EventArgs e) {
+    var results = GenerateStudentReport();
+    dgvResults.DataSource = results;
+}var form = new FrmFinalReport();
+form.ShowDialog();
